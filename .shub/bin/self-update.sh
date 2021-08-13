1850e0f54583cc75748209762205d45ed256f3f5
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-13 01:03:42 -0300

    chore: update shub scripts

--- .shub/bin/self-update.sh
+++ .shub/bin/self-update.sh
@@ -10,9 +10,12 @@ if [ "$REMOTE_VERSION" != "$LOCAL_VERSION" ]; then
         response=${response,,} # tolower
         if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
             echo "Updating..."
-            curl -o .shub/bin/version --create-dirs https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/version
-            curl -o .shub/bin/deploy.sh --create-dirs https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/deploy.sh
+            curl -o .shub/bin/version https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/version
+            curl -o .shub/bin/deploy.sh https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/deploy.sh
+            curl -o .shub/bin/self-update.sh https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/self-update.sh
             chmod +x .shub/bin/deploy.sh
-            exit 0;
+            exit 0
         fi
 fi
+
+exit 1
